80dc9c42547a08575a6e15bb713d289fe235eeda
Garrett Tichy <garrett.tichy@gmail.com>
2012-10-24 07:26:02 -0400

    Version 1.0
    Compatible to WordPress 3.4.2

---
+++ js/announce.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function($) {
+    if($('#sap_start_date').length) {
+        $(function() {
+            var pickerOpts = {
+                dateFormat: "yy-mm-dd"
+            };
+            jQuery("#sap_start_date").datepicker(pickerOpts);
+            jQuery("#sap_end_date").datepicker(pickerOpts);
+        });
+    }
+
+        
+    if($('#announcements').length) {
+    	if($.cookie('sap_active') == 'false') {
+        	$("#announcements").hide();
+        	}
+        	$("#close").click(function() {
+	        	$("#announcements").slideUp("normal");
+	        	$.cookie('sap_active', 'false', { expires: 2, path: '/'});
+	        	return false;
+	        	});
+	
+	$("body").prepend($("#announcements"));
+    $('#announcements .sap_message').cycle('fade');
+}
+
+});
